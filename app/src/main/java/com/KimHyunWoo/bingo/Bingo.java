fa875a2e9a2c57df059f18e0efb9f889d8f56664
Kim-2sam2 <Kim-2sam2@github.com>
2019-05-26 23:38:51 +0900

    컴퓨터 코딩 완료. 테스트 안함

--- app/src/main/java/com/KimHyunWoo/bingo/Bingo.java
+++ app/src/main/java/com/KimHyunWoo/bingo/Bingo.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -15,6 +16,8 @@ public class Bingo extends AppCompatActivity {
 
     setMatrix player, computer;
     private int playerClickNumber= -1, computerClickNumber= -1;
+    private final int X = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,13 +32,16 @@ public class Bingo extends AppCompatActivity {
             }
         }
 
+        /*컴퓨터와 플레이어 빙고판 선언*/
         computer = new setMatrix();
         player = new setMatrix();
+
         for(int i = 0; i<5; i++){
             for(int j =0; j<5; j++){
                 matrix[i][j].setText(String.valueOf(player.nMatrix[i][j]));
             }
         }
+
         clickCell();
 
     }
@@ -46,9 +52,13 @@ public class Bingo extends AppCompatActivity {
                 matrix[i][j].setOnClickListener(new findIndexOnClickListener(i,j) {
                     @Override
                     public void onClick(View v) {
-                        playerClickNumber = Integer.parseInt(matrix[i][j].getText().toString());
-                        matrix[i][j].setText("X");
-                        //todo X일때 토스트
+                        if(player.nMatrix[i][j] == -1){
+                            Toast.makeText(Bingo.this, "이미 입력한 번호 입니다.", Toast.LENGTH_SHORT).show();
+                        }else{
+                            playerClickNumber = player.nMatrix[i][j];
+                            player.nMatrix[i][j] = -1;
+                            matrix[i][j].setText("X");
+                        }
                         //todo 숫자 눌렀을때 턴 넘기기
                     }
                 });
@@ -60,37 +70,15 @@ public class Bingo extends AppCompatActivity {
         for(int i = 0; i<5; i++){
             for(int j = 0; j<5; j++){
                 if(player.nMatrix[i][j] == playerClickNumber){
-                    player.nMatrix[i][j] = -1;
+                    player.nMatrix[i][j] = X;
                     matrix[i][j].setText("X");
                 }
                 if(computer.nMatrix[i][j] == computerClickNumber){
-                    computer.nMatrix[i][j] = -1;
+                    computer.nMatrix[i][j] = X;
                 }
             }
         }
     }
 
-    private void computerTurn(){
-
-        /* 마킹 갯수*/
-        int dia1=0, dia2=0; //dia1: 오른쪽상단 to 왼쪽하단 대각선, dia2: 왼쪽상단 to 오른쪽하단 대각선
-        int[] wid = new int[5];
-        int[] hei = new int[5];
-
-        //가운데 우선//
-        if(computer.nMatrix[2][2] != -1){
-            computer.nMatrix[2][2] = computerClickNumber;
-            computer.nMatrix[2][2] = -1;
-        }else{
-            for(int i =0; i<5; i++){
-                for(int j= 0; j<5; j++){
-                    
-                }
-            }
-        }
-
-
-
-    }
 
 }
--- app/src/main/java/com/KimHyunWoo/bingo/setMatrix.java
+++ app/src/main/java/com/KimHyunWoo/bingo/setMatrix.java
@@ -1,14 +1,25 @@
 package com.KimHyunWoo.bingo;
 
-import android.view.View;
-import android.widget.TextView;
+import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class setMatrix {
 
+    private final String TAG = "Bingo";
     private final int max = 51; //0~50
     int [][] nMatrix = new int[5][5];
+
+    /* 마킹 갯수*/
+    int dia1=0, dia2=0; //dia1: 오른쪽상단 to 왼쪽하단 대각선, dia2: 왼쪽상단 to 오른쪽하단 대각선
+    int[] wid = new int[5];
+    int[] hei = new int[5];
+    int count=0;
+
+    private final int X = -1;
+    protected int computerClickNumber=-1;
 
     setMatrix(){
         int num;
@@ -36,5 +47,231 @@ public class setMatrix {
         return true;
     }
 
+    public int getComputerClickNumber(){
+        return this.computerClickNumber;
+    }
 
+    protected void computerTurn(){
+
+        searchMark();
+        findDirection();
+
+    }
+
+    private void searchMark(){
+        //가운데 우선//
+        if(nMatrix[2][2] != X){
+            nMatrix[2][2] = computerClickNumber;
+            nMatrix[2][2] = X;
+            Log.d(TAG,"com: " + computerClickNumber);
+        }else{
+            for(int i =0; i<5; i++) {
+                if (nMatrix[i][i] == X) dia1++;
+            }
+
+            for(int i = 4; i>=0; i--){
+                if(nMatrix[4-i][i] == X) dia2++;
+            }
+
+            for(int i = 0; i<5; i++){
+                count = 0;
+                for(int j=0; j<5; j++){
+                    if(nMatrix[i][j] == X) count++;
+                }
+                wid[i]=count;
+            }
+
+            for(int i = 0; i<5; i++){
+                count = 0;
+                for(int j=0; j<5; j++){
+                    if(nMatrix[j][i] == X) count++;
+                }
+                hei[i]=count;
+            }
+        }
+    }
+
+    private void findDirection(){
+        int index=0;
+        ArrayList<Integer> arr= new ArrayList<>();
+
+        //5일경우 빙고 완성이므로 제외
+        if(dia1 == 5) dia1 = -2;
+        if(dia2 == 5) dia2 = -2;
+        arr.add(dia1);
+        arr.add(dia2);
+        for(Integer temp : hei){
+            if(temp == 5) temp = -2;
+            arr.add(temp);
+        }
+        for(Integer temp : wid){
+            if(temp == 5) temp = -2;
+            arr.add(temp);
+        }
+
+        //max: 구역 중 제일 큰 값
+        //max값을 가진 구역을 찾는다. 1순위. 대각선, 2순위. 가로, 3순위. 세로
+
+        while(index <5){
+            if(dia1 == max){
+                findPosition("dia1", index);
+                Log.d(TAG,"com: Target-dia1");
+                break;
+            }else if(dia2 == max){
+                findPosition("dia2", index);
+                Log.d(TAG,"com: Target-dia2");
+                break;
+            }else if(wid[index] == max){
+                findPosition("wid", index);
+                Log.d(TAG,"com: Target-wid");
+                break;
+            }else if(hei[index] == max){
+                findPosition("hei", index);
+                Log.d(TAG,"com: Target-hei");
+                break;
+            }else{
+                index++;
+            }
+        }
+    }
+
+    //정해진 구역에서 어디에 마킹할지 찾는다.
+    private void findPosition(String target, int index){
+        ArrayList<Integer> arr= new ArrayList<>();
+        //int[] arr = new int[5];
+        int mark=-1;
+        switch(target){
+            case "dia1":
+                for(int i=0; i<5; i++){
+                    if(nMatrix[i][i] != X){
+                        arr.add(checkCross(i,i));
+                    }else{
+                        arr.add(0);
+                    }
+                }
+                marking(arr,target, index);
+                break;
+
+            case "dia2":    //arr: [4][0] / [3][1] / [2][2] / [1][3] / [0][4] 순서
+                for(int i=4; i>=0; i--){
+                    if(nMatrix[4-i][i] != X){
+                        arr.add(checkCross(4-i,i));
+                    }else{
+                        arr.add(0);
+                    }
+                }
+                marking(arr,target, index);
+                break;
+
+            case "wid":
+                for(int i=0; i<5; i++){
+                    if(nMatrix[index][i] != X){
+                        arr.add(checkCross(index,i));
+                    }else{
+                        arr.add(0);
+                    }
+                }
+                marking(arr,target,index);
+                break;
+
+            case "hei":
+                for(int i=0; i<5; i++){
+                    if(nMatrix[i][index]!=X){
+                        arr.add(checkCross(i,index));
+                    }else{
+                        arr.add(0);
+                    }
+                }
+                marking(arr,target,index);
+                break;
+
+        }
+    }
+
+    //각 구역 5개 요소의 가로 세로에 있는 x 갯수를 비교하여 많은 위치에 마킹
+    private int  checkCross(int wid, int hei){
+        int n_wid=0, n_hei=0;
+
+        //가로//
+        for(int i =0; i<5; i++){
+            if(nMatrix[wid][i]==X) n_wid++;
+        }
+
+        //세로
+        for(int i  =0; i<5; i++){
+            if(nMatrix[i][hei] == X) n_hei++;
+        }
+
+        return n_wid + n_hei;
+    }
+
+
+    //target_index: 각 타겟의 인덱스 => 타겟: 대각선, 가로, 세로
+    //즉, 타겟이 가로이고 target_index = 1이라면 nMatrix[target_index][N] 이 된다. => 가로 target_index번째 줄
+    private void marking(ArrayList<Integer> arr, String target, int target_index){
+        int max = Collections.max(arr);
+
+        int index = arr.indexOf(max);
+
+        switch(target){
+            case "dia1":
+                if(nMatrix[index][index]==X){
+                    for(int i=index+1; i<5; i++){
+                        if(max>arr.get(i)){
+                            max=arr.get(i);
+                        }
+                    }
+                    computerClickNumber = nMatrix[arr.indexOf(max)][arr.indexOf(max)];
+                    nMatrix[arr.indexOf(max)][arr.indexOf(max)] = -1;
+                }else{
+                    computerClickNumber = nMatrix[index][index];
+                    nMatrix[index][index] = -1;
+                }
+                break;
+            case "dia2":
+                if(nMatrix[4-index][index]==X){
+                    for(int i=index+1; i<5; i++){
+                        if(max>arr.get(i)){
+                            max=arr.get(i);
+                        }
+                    }
+                    computerClickNumber = nMatrix[4-arr.indexOf(max)][arr.indexOf(max)];
+                    nMatrix[4-arr.indexOf(max)][arr.indexOf(max)] = -1;
+                }else{
+                    computerClickNumber = nMatrix[4-index][index];
+                    nMatrix[4-index][index] = -1;
+                }
+                break;
+
+            case "wid":
+                if(nMatrix[target_index][index]==X){
+                    for(int i= index+1; i<5; i++){
+                        if(max>arr.get(i)){
+                            max = arr.get(i);
+                        }
+                    }
+                    computerClickNumber = nMatrix[target_index][arr.indexOf(max)];
+                    nMatrix[target_index][arr.indexOf(max)] = -1;
+                }else{
+                    computerClickNumber = nMatrix[target_index][index];
+                    nMatrix[target_index][index] = -1;
+                }
+                break;
+
+            case "hei":
+                if(nMatrix[index][target_index] == X){
+                    for(int i=index+1; i<5; i++){
+                        if(max>arr.get(i)){
+                            max = arr.get(i);
+                        }
+                    }
+                    computerClickNumber = nMatrix[arr.indexOf(max)][target_index];
+                    nMatrix[arr.indexOf(max)][target_index] = -1;
+
+                }else{
+                    computerClickNumber = nMatrix[index][target_index];
+                    nMatrix[index][target_index] = -1;
+                }
+        }
+    }
 }
